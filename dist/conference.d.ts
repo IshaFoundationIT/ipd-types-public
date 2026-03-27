6ebf3638a81fdd603598f6350c3a3d3468a76d85
Jeevan Anga <jeevanraj.angamuthu@sadhguru.org>
2026-03-27 18:35:33 +0530

    Revert "chore: rename package to @ishafoundationit/ipd-types, commit dist"
    This reverts commit 468a002bcdce6740a236518034e7a8e3e55d2964.

--- dist/conference.d.ts
+++
@@ -1,9 +0,0 @@
-export type ConfSessionEligibleHostOptions = ConfSessionEligibleHostOption[];
-export interface ConfSessionEligibleHostOption {
-    ssoId: string;
-    email: string;
-}
-export interface ConfSessionCoHostOption {
-    ssoId: string;
-    email: string;
-}
